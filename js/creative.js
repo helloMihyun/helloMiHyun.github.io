fa5ca0614ec11adbc670920891cf8442f44b4694
MiHyun <sky1318i@gmail.com>
2021-09-08 18:15:28 +0900

    Update creative.js

--- js/creative.js
+++ js/creative.js
@@ -94,14 +94,14 @@ var data = [
   {
     username:"Osulloc", 
     // Key "username" means that Magnific Popup will look for an element with class "mfp-username" in markup and will replace its inner HTML with the value.
-    userWebsite_href: 'yamyamtomato.github.io/01_osulloc', // Key "userWebsite_href" means that Magnific Popup will look fo`r an element with class "mfp-userWebsite" and will change its "href" attribute. Instead of ending "href" you may put any other attribute.
+    userWebsite_href: 'yamyamtomato.github.io/01_osulloc/index.html', // Key "userWebsite_href" means that Magnific Popup will look fo`r an element with class "mfp-userWebsite" and will change its "href" attribute. Instead of ending "href" you may put any other attribute.
     userAvatarUrl_img: 'img/portfolio/fullsize/1.jpg', 
       // Prefix "_img" is special. With it Magnific Popup finds an  element "userAvatarUrl" and replaces it completely with image tag.
     explan:'2016년도 오설록 홈페이지를 photoshop과 illust를 사용하여 리뉴얼 디자인하고 html, css, j-query를 사용하여 제작하였습니다.<br />기업형 페이지로 제작하여 메인이 되는 페이지와 서브페이지 2장으로 구성되어 있습니다.<br />' 
   },
   {
     username:"hanssem",
-    userWebsite_href: 'yamyamtomato.github.io/02_hanssem',
+    userWebsite_href: 'yamyamtomato.github.io/02_hanssem/index.html',
     userAvatarUrl_img:'img/portfolio/fullsize/2.jpg',
     explan:'한샘닷컴의 홈페이지를 저만의 코드로 구현하여 제작하였습니다.<br />기업형 페이지로 제작하여 메인이 되는 페이지와 서브페이지 2장으로 구성되어 있습니다.'
 
@@ -112,25 +112,25 @@ var data = [
     // userAvatarUrl_img:'img/portfolio/fullsize/3.jpg',
     // explan:'반스 홈페이지를 photoshop과 illust를 사용하여 리뉴얼 디자인하고 html, css, j-qeury를 사용하여 제작하였습니다.<br />원페이지로 구성된 사이트로 반응형이 가능한 사이트로 제작하였습니다.'
     username:"vans",
-    userWebsite_href: 'yamyamtomato.github.io/03_vans',
+    userWebsite_href: 'yamyamtomato.github.io/03_vans/index.html',
     userAvatarUrl_img:'img/portfolio/fullsize/3.jpg',
     explan:'사이트 업데이트 중 입니다.' 
   },
   {
     username:"freitag",
-    userWebsite_href: 'yamyamtomato.github.io/04_freitag',
+    userWebsite_href: 'https://yamyamtomato.github.io/04_freitag/index.html',
     userAvatarUrl_img:'img/portfolio/fullsize/4.jpg',
     explan: '프라이탁의 홈페이지를 저만의 코드로 구현하여 제작하였습니다.<br />원페이지로 구성된 사이트로 반응형이 가능한 사이트로 900px과 699px의 화면에 맞는 layout으로 제작하였습니다. '
   },
   {
     username:"FransHals Museum",
-    userWebsite_href: 'yamyamtomato.github.io/05_franshalsmuseum',
+    userWebsite_href: 'https://yamyamtomato.github.io/05_franshalsmuseum/index.html',
     userAvatarUrl_img:'img/portfolio/fullsize/5.jpg',
     explan:'"FransHals Museum" 홈페이지를 저만의 코드로 구현하여 제작하였습니다.<br />원페이지로 구성된 사이트로 콘텐츠 부분을 좌우로 나눠 각각의 컨텐츠 내용을 확인 할 수 있습니다.'
   },
   {
     username: "Administrator Page / Basic Form UI & UI Elements",
-    userWebsite_href: 'yamyamtomato.github.io/06_admin',
+    userWebsite_href: 'https://yamyamtomato.github.io/06_admin/index.html',
     userAvatarUrl_img:'img/portfolio/fullsize/6.jpg',
     explan:'사이트의 통일성과 작업 속도 향상을 위해 정확한 가이드 클래스를 모듈화하여 폼, 버튼, 레이아웃 서식 등을 제작하여 관리가 용이하도록 만들어 두었습니다.'
   }
